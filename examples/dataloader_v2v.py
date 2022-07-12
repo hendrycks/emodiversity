b29cba280390dfbf29413860299797ef009929c8
JunShern Chan <junshern@JunSherns-MacBook-Pro.local>
2022-07-12 13:52:54 -0700

    Add v2v dataloader, implement frame selection for exact frame count

--- examples/dataloader_v2v.py
+++ examples/dataloader_v2v.py
@@ -36,7 +36,7 @@ def loadvideo_decord(fname, width, height, num_frames, frame_sample_rate=1):
     buffer = vr.get_batch(chosen_idxs).asnumpy()
     return buffer
 
-class VCEDataset(Dataset):
+class V2VPairwiseDataset(Dataset):
 
     def __init__(self, dataset_path, split, video_width, video_height, num_frames):
         assert split == "train" or split == "test"
@@ -49,45 +49,44 @@ class VCEDataset(Dataset):
         with open(self.data_path / "metadata.json") as f:
             metadata_dict = json.load(f)
         
-        self.video_paths = []
-        self.labels = []
-        self.metadata = []
+        self.video_paths_1 = []
+        self.video_paths_2 = []
+        self.metadata_1 = []
+        self.metadata_2 = []
         with open(self.data_path / f"{split}_labels.json") as f:
             labels_dict = json.load(f)
-            for key, obj in labels_dict.items():
-                path, label = self.get_path_and_label(obj)
-                self.video_paths.append(path)
-                self.labels.append(label)
-                self.metadata.append(metadata_dict[key])
+            for comparison_list in labels_dict["comparisons"]:
+                video_id_1, video_id_2 = comparison_list
+                path_1 = str(self.data_path / f"videos/{video_id_1}.mp4")
+                path_2 = str(self.data_path / f"videos/{video_id_2}.mp4")
 
-        assert len(self.video_paths) == len(self.metadata)
-        assert len(self.video_paths) == len(self.labels)
+                self.video_paths_1.append(path_1)
+                self.video_paths_2.append(path_2)
+                self.metadata_1.append(metadata_dict[video_id_1])
+                self.metadata_2.append(metadata_dict[video_id_2])
+
+        assert len(self.video_paths_1) == len(self.metadata_1)
+        assert len(self.video_paths_2) == len(self.metadata_2)
+        assert len(self.video_paths_1) == len(self.video_paths_2)
 
     def __len__(self):
-        return len(self.video_paths)
+        return len(self.video_paths_1)
 
     def __getitem__(self, idx):
-        video = loadvideo_decord(self.video_paths[idx], self.video_width, self.video_height, self.num_frames)
-        label = self.labels[idx]
-        md = self.metadata[idx]
-        return video, label, md
-
-    def get_path_and_label(self, label_obj):
-        # Convert from 27-vector of emotion scores to a single classification label corresponding to the max-scoring emotion
-        emotions_and_scores = sorted(list(label_obj["emotions"].items())) # Make sure they are sorted alphabetically
-        scores = [score for emotion, score in emotions_and_scores]
-        label = int(np.argmax(scores))
-        path = str(self.data_path / label_obj['file'])
-        return path, label
+        video_1 = loadvideo_decord(self.video_paths_1[idx], self.video_width, self.video_height, self.num_frames)
+        video_2 = loadvideo_decord(self.video_paths_2[idx], self.video_width, self.video_height, self.num_frames)
+        md_1 = self.metadata_1[idx]
+        md_2 = self.metadata_2[idx]
+        return video_1, video_2, md_1, md_2
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='PyTorch Dataset and DataLoader for VCE dataset')
+    parser = argparse.ArgumentParser(description='PyTorch Dataset and DataLoader for V2V dataset')
     parser.add_argument("-d", "--data-dir", required=True, help="Dataset directory.")
     options = parser.parse_args()
 
-
-    full_dataset = VCEDataset(options.data_dir, split="train", video_width=320, video_height=256, num_frames=10)
+    
+    full_dataset = V2VPairwiseDataset(options.data_dir, split="train", video_width=320, video_height=256, num_frames=10)
     print("Length of dataset", len(full_dataset))
     train_size = int(0.8 * len(full_dataset))
     val_size = len(full_dataset) - train_size
@@ -95,12 +94,13 @@ if __name__ == '__main__':
 
 
     def collate_batch(batch):
-        videos, labels, metadata = [], [], []
-        for (video, label, md) in batch:
-            videos.append(video)
-            labels.append(label)
-            metadata.append(md)
-        return videos, labels, metadata
+        videos_1, videos_2, metadata_1, metadata_2 = [], [], [], []
+        for (video_1, video_2, md_1, md_2) in batch:
+            videos_1.append(video_1)
+            videos_2.append(video_2)
+            metadata_1.append(md_1)
+            metadata_2.append(md_2)
+        return videos_1, videos_2, metadata_1, metadata_2
 
     batch_size = 64
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0, collate_fn=collate_batch)
@@ -108,6 +108,7 @@ if __name__ == '__main__':
 
     for batch_idx, batch in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
         print("Batch", batch_idx)
-        videos, labels, metadata = batch
-        for vid, md, label in zip(videos, metadata, labels):
-            print(vid.shape, md, ">> Label:", label)
+        videos_1, videos_2, metadata_1, metadata_2 = batch
+        for vid_1, vid_2, md_1, md_2 in zip(videos_1, videos_2, metadata_1, metadata_2):
+            print("vid 1:", vid_1.shape, md_1)
+            print("vid 2:", vid_2.shape, md_2)
